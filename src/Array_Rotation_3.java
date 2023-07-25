3e5580a6a89284ab7f0c475b901c61ff8de924e0
jeevaa_the_coder <jeevaathecoder@gmail.com>
2023-07-25 21:15:37 +0530

    Some Changes Made

--- src/Array_Rotation_3.java
+++ src/Array_Rotation_3.java
@@ -27,7 +27,20 @@ public class Array_Rotation_3 {
         System.out.println(Arrays.toString(arr));
             }
 
+            //Left Shift
+//    public static void main(String[] args) {
+//        int[] arr= {1 ,2 ,3 ,4, 5};
+//
+//        int first = arr[0];
+//
+//        for(int i= 1 ; i< arr.length ; i++){
+//            arr[i-1] = arr[i];
+//        }
+//        arr[arr.length-1] =first;
+//        System.out.println(Arrays.toString(arr));
 
 
     }
+
+
 
